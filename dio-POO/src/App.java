683bad32b062ca9c8934edec414b3b172c2fca5d
alvesgeraldo <jgalvesb@gmail.com>
2022-11-21 15:27:19 -0300

    Criaçao das classes Mentoria e Curso

--- dio-POO/src/App.java
+++ dio-POO/src/App.java
@@ -1,5 +1,28 @@
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+      Curso curso1 = new Curso();
+      curso1.setTitulo("Curso Java");
+      curso1.setDescricao("Descriço Curso Java");
+      curso1.setCargaHoraria(8);
+
+      Curso curso2 = new Curso();
+      curso2.setTitulo("Curso JS");
+      curso2.setDescricao("Descriço Curso JS");
+      curso2.setCargaHoraria(4);
+
+      Mentoria mentoria = new Mentoria();
+      mentoria.setTitulo("Mentoria curso java");
+      mentoria.setDescricao("Descriçao da mentoria java");
+      mentoria.setData(LocalDate.now());
+
+      System.out.println(curso1);
+      System.out.println(curso2);
+      System.out.println(mentoria);
     }
 }
